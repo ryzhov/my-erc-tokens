7fe33285cf549c4a335bd1a6b59db01933420726
labintsev <ailabintsev@fa.ru>
2024-12-04 22:57:52 +0300

    fix ERC20 some test

--- packages/hardhat/test/YourContract.ts
+++ packages/hardhat/test/YourContract.ts
@@ -7,22 +7,18 @@ describe("YourContract", function () {
 
   let yourContract: YourContract;
   before(async () => {
-    const [owner] = await ethers.getSigners();
     const yourContractFactory = await ethers.getContractFactory("YourContract");
-    yourContract = (await yourContractFactory.deploy(owner.address)) as YourContract;
+    yourContract = (await yourContractFactory.deploy(1000)) as YourContract;
     await yourContract.waitForDeployment();
   });
 
   describe("Deployment", function () {
-    it("Should have the right message on deploy", async function () {
-      expect(await yourContract.greeting()).to.equal("Building Unstoppable Apps!!!");
+    it("Should have the right symbol on deploy", async function () {
+      expect(await yourContract.symbol()).to.equal("IWB");
     });
 
-    it("Should allow setting a new message", async function () {
-      const newGreeting = "Learn Scaffold-ETH 2! :)";
-
-      await yourContract.setGreeting(newGreeting);
-      expect(await yourContract.greeting()).to.equal(newGreeting);
+    it("Should allow read contract supply", async function () {
+      expect(await yourContract.totalSupply()).to.equal(1000);
     });
   });
 });
