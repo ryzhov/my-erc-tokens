6c0c4d8e3a3c6f5318b0ac89c6b62f574c818515
Andrej <andrej.labintsev@yandex.ru>
2024-12-05 20:41:36 +0300

    modify layout, add voting stub

---
+++ packages/nextjs/app/voting/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <>
+      <h1>Voting Page</h1>
+    </>
+  );
+}
--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,7 +4,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -20,9 +20,16 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/",
   },
   {
-    label: "Debug Contracts",
+    label: "Votings",
+    href: "/voting",
+  },
+  {
+    label: "Contract API",
     href: "/debug",
-    icon: <BugAntIcon className="h-4 w-4" />,
+  },
+  {
+    label: "Block Explorer",
+    href: "/blockexplorer",
   },
 ];
 
@@ -93,8 +100,8 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Indie Wood</span>
+            <span className="text-xs">Smart bonds</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
