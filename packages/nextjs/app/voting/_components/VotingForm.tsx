c917c377045c51d2674c533821a17c4cf228bf44
labintsev <ailabintsev@fa.ru>
2024-12-13 17:36:27 +0300

    add voting margin

--- packages/nextjs/app/voting/_components/VotingForm.tsx
+++ packages/nextjs/app/voting/_components/VotingForm.tsx
@@ -7,7 +7,7 @@ export default function VotingForm({ question }: { question: Question }) {
       <div className="items-left m-8">
         <div>{question.question}</div>
         {question.choices_array.map(choice => (
-          <div className="flex items-center mb-4" key={choice}>
+          <div className="flex items-center my-4" key={choice}>
             <label className="label cursor-pointer ms-2">
               <input type="radio" name="radio-0" value={choice} className="radio checked:bg-red-500" />
               <span className="ms-2">{choice}</span>
